2beaaad397c869963885fe4f87964829c4b66c28
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-12-09 16:07:26 +0300

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -2,13 +2,27 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
-end
+ActiveRecord::Schema.define(version: 2019_04_07_161357) do
+
+    # These are extensions that must be enabled in order to support this database
+    enable_extension "plpgsql"
+  
+    create_table "recipes", force: :cascade do |t|
+      t.string "name", null: false
+      t.text "ingredients", null: false
+      t.text "instruction", null: false
+      t.string "image", default: "https://raw.githubusercontent.com/9jaswag/react_rails_recipe/master/app/assets/images/sample_dish.jpg"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+    end
+  
+  end
+  
